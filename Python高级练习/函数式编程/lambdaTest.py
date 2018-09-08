7062d282fa46b0a30f9135e2656e1ac1310e810b
ITianerU <ITianerU@users.noreply.github.com>
2018-09-08 09:20:47 +0800

    lambda表达式

---
+++ Python高级练习/函数式编程/lambdaTest.py
@@ -0,0 +1,7 @@
+#lambda表达式
+
+a = lambda x:x*100
+print(a(10))
+
+b = lambda x,y:x+y
+print(b(100,10))
---
+++ Python高级练习/包/test.py
@@ -0,0 +1,11 @@
+# from 包 import *
+#
+# a = student.student()
+# # a.say()
+#
+# sayHello()
+
+from 包.student import *
+
+a = student()
+a.say()
