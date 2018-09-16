b4ecbaceaf03f10364bbe0a721b5f2b26967a9e2
ITianerU <ITianerU@users.noreply.github.com>
2018-09-16 20:58:11 +0800

    etree创建xml

---
+++ Python高级练习/xml解析/EtreeCreateXmlTest.py
@@ -0,0 +1,13 @@
+#使用etree创建xml
+import xml.etree.ElementTree as xee
+
+
+toy = xee.Element("toy")
+car = xee.Element("car")
+# car = xee.SubElement(toy,"car")
+car.attrib = {"height":"1.5","width":"3"}
+car.text = "劳斯莱斯-魅影"
+toy.append(car)
+
+xee.dump(toy)
+
