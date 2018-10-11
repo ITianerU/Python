8715ad7239c289a17e527432d80a350f7da16478
ITianerU <1438687182@qq.com>
2018-10-11 23:42:40 +0800

    两道练习题 1.素数生成器 2.筛选回文数

--- Python高级练习/函数式编程/FilterTest.py
+++ Python高级练习/函数式编程/FilterTest.py
@@ -7,3 +7,49 @@ fil = filter(guoLu,[1,4,7,5,4,7,8])
 #和map一样只能使用一次
 print([i for i in fil])
 
+#素数的生成器-----埃氏筛法
+def _odd_iter():
+    n = 1
+    while True:
+        n += 2
+        yield n
+
+def primes():
+    yield 2
+    it = _odd_iter()
+    def _not_divisible(n):
+        return lambda x:x%n > 0
+    while True:
+        n = next(it)
+        yield n
+        it = filter(_not_divisible(n),it)
+
+for i in primes():
+    if i<1000:
+        print(i)
+    else:
+        break
+
+
+#筛选回数
+def is_palindrome(n):
+    def fn(n):
+        n = str(n)
+        length = len(n)
+        if length > 0:
+            if length%2==0:
+                if n[0:length//2]==n[length//2:]:
+                    return True
+                else:
+                    return False
+            else:
+                if n[0:length//2]==n[length//2+1:]:
+                    return True
+                else:
+                    return False
+        else:
+            return True
+    return filter(fn,n)
+
+
+print(list(is_palindrome(range(1000))))
