dbfb634f13a26407c8d2a242b29bb929d15e1f93
ITianerU <1438687182@qq.com>
2018-10-11 22:29:41 +0800

    三道练习题 1.规范用户名,首字母大写,其余字母小写 2.求积函数 3.将字符串转换为浮点数

--- Python高级练习/函数式编程/MapTest.py
+++ Python高级练习/函数式编程/MapTest.py
@@ -17,4 +17,42 @@ c = [i for i in b]
 print(c)
 
 
-#测试pycharm git two
+#利用map()函数，把用户输入的不规范的英文名字，变为首字母大写，其他小写的规范名字。输入：['adam', 'LISA', 'barT']，输出：['Adam', 'Lisa', 'Bart']：
+def normalize(name):
+    def fn(x):
+        return x[0].upper()+x[1:].lower()
+    return map(fn,name)
+L1 = ['adam', 'LISA', 'barT']
+print(list(normalize(L1)))
+
+#Python提供的sum()函数可以接受一个list并求和，请编写一个prod()函数，可以接受一个list并利用reduce()求积：
+from functools import reduce
+def prod(L):
+    def fn(x,y):
+        return x*y
+    return reduce(fn,L)
+print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
+if prod([3, 5, 7, 9]) == 945:
+    print('测试成功!')
+else:
+    print('测试失败!')
+
+#利用map和reduce编写一个str2float函数，把字符串'123.456'转换成浮点数123.456：
+DIGITS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+def str2float(s):
+    num1 = s.split(".")[0]
+    num2 = sorted(s.split(".")[1],reverse=True)
+    def fn1(x,y):
+        return x*10+y
+    def fn2(x,y):
+        return x*0.1+y
+    def fn3(x):
+        return DIGITS[x]
+    return reduce(fn1,map(fn3,num1))+reduce(fn2,map(fn3,num2))*0.1
+print('str2float(\'123.456\') =', str2float('123.456'))
+if abs(str2float('123.456') - 123.456) < 0.00001:
+    print('测试成功!')
+else:
+    print('测试失败!')
+
+
