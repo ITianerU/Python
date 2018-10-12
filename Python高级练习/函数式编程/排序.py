61c5d3318a533a3ef941e7efc3b97899759f793a
ITianerU <1438687182@qq.com>
2018-10-12 21:36:08 +0800

    sort练习题:对tuple组成的list排序

--- Python高级练习/函数式编程/排序.py
+++ Python高级练习/函数式编程/排序.py
@@ -19,3 +19,14 @@ print(d)
 e = sorted(['Ab','a','d','c'],key=str.lower)
 print(e)
 
+#假设我们用一组tuple表示学生名字和成绩：
+#用sorted()对上述列表分别按名字排序：
+def by_name(t):
+    return t[0]
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+print(sorted(L,key=by_name))
+
+#再按成绩从高到低排序：
+def by_score(t):
+    return -t[1]
+print(sorted(L,key=by_score))
