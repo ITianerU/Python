7f2680edec6a01c58550c962247752fd87df537f
ITianerU <1438687182@qq.com>
2018-10-14 23:08:26 +0800

    @property练习

---
+++ Python高级练习/包/模块/类@propertyTest.py
@@ -0,0 +1,28 @@
+#请利用@property给一个Screen对象加上width和height属性，以及一个只读属性resolution：
+class Screen(object):
+    @property
+    def width(self):
+        return self._width
+    @width.setter
+    def width(self,width):
+        self._width = width
+
+    @property
+    def height(self):
+        return self._height
+    @height.setter
+    def height(self,height):
+        self._height = height
+    @property
+    def resolution(self):
+        return self._width*self._height
+
+# 测试:
+s = Screen()
+s.width = 1024
+s.height = 768
+print('resolution =', s.resolution)
+if s.resolution == 786432:
+    print('测试通过!')
+else:
+    print('测试失败!')
