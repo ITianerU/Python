570a24dc772e0202e973c5d46ab5bf96469dc15d
ITianerU <1438687182@qq.com>
2018-10-15 21:53:30 +0800

    异常处理练习

--- Python高级练习/异常/test.py
+++ Python高级练习/异常/test.py
@@ -22,3 +22,24 @@ try:
 except lrtError as e:
     print(e)
 
+from functools import reduce
+
+
+def str2num(s):
+    if type(s) == int:
+        return int(s)
+    else:
+        return float(s)
+
+def calc(exp):
+    ss = exp.split('+')
+    ns = map(str2num, ss)
+    return reduce(lambda acc, x: acc + x, ns)
+
+def main():
+    r = calc('100 + 200 + 345')
+    print('100 + 200 + 345 =', r)
+    r = calc('99 + 88 + 7.6')
+    print('99 + 88 + 7.6 =', r)
+
+main()
