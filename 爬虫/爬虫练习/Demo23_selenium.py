1f45b48a6c9221350f86959e9dcd583870d6d70f
ITianerU <1438687182@qq.com>
2018-11-05 22:00:25 +0800

    selenium

---
+++ 爬虫/爬虫练习/Demo23_selenium.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.get("https://www.baidu.com")
+print(driver.title)
