4788be54869e5ee73ea01244af56e1652d117bad
ITianerU <1438687182@qq.com>
2019-03-16 17:39:28 +0800

    scrapy练习

---
+++ 爬虫/爬虫练习2/Scrapy/scrapydownloadertest/scrapydownloadertest/spiders/httpbin.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class HttpbinSpider(scrapy.Spider):
+    name = 'httpbin'
+    allowed_domains = ['httpbin.org']
+    start_urls = ['http://httpbin.org/get']
+
+    def parse(self, response):
+        # 日志输出
+        self.logger.debug(response.text)
+        self.logger.debug('Status Code:'+str(response.status))
